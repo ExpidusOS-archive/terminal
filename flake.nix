6fbf1f95072083eb66f84fe7562ea093edb1e99c
Tristan Ross <tross@ezoic.com>
2022-11-09 14:49:03 -0800

    feat: add ci

--- flake.nix
+++ flake.nix
@@ -5,5 +5,5 @@
     url = github:ExpidusOS/sdk;
   };
 
-  outputs = { self, expidus-sdk }: expidus-sdk.lib.mkFlake { inherit self; name = "expidus-terminal"; };
+  outputs = { self, expidus-sdk }: expidus-sdk.libExpidus.mkFlake { inherit self; name = "expidus-terminal"; };
 }
